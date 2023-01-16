34e4eb45fe0595b4b610df682b5fb557f0548321
bertrand-coez <coez.bertrand@gmail.com>
2023-01-16 19:37:51 +0100

    new type improved class

--- src/promises/index.ts
+++ src/promises/index.ts
@@ -12,28 +12,14 @@ const DEFAULT_OPTIONS: RetryOptions = {
 	factor: 2
 };
 
-export interface Retry<T extends (...args: any) => Promise<any>>{
-	target: T;
-	parameters: Parameters<T>,
-	options: RetryOptions;
-	event: EventEmitter;
-	
-	new(target: T, parameters: Parameters<T>, options: Partial<RetryOptions>): void;
-	try(tries: number): Promise<T>;
-	updateInterval(): number;
-	on(event: "failure", cb: (error: any, countdown: number, currentInterval: number) => void): this
-	on(event: "retry", cb: (countdown: number, currentInterval: number) => void): this
-	once(event: "failure", cb: (error: any, countdown: number, currentInterval: number) => void): this
-	once(event: "retry", cb: (countdown: number, currentInterval: number) => void): this
-}
 
 /**Allows you to retry an async function while it fails until the max number of tries have been reached. */
-export class Retry<T extends (...args: any) => Promise<any>> {
+export class Retry< T extends (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> > {
 	private _target: T;
 	private _parameters: Parameters<T>;
 	private _options: RetryOptions;
 	private _event: EventEmitter;
-
+	
 	/**
 	 * Allows you to retry an async function while it fails until the max number of tries have been reached
 	 * @param target async function to execute until it succeeds.
@@ -42,7 +28,7 @@ export class Retry<T extends (...args: any) => Promise<any>> {
 	constructor(target: T, parameters: Parameters<T>, options: Partial<RetryOptions> = {}){
 		this._target = target;
 		this._parameters = parameters;
-		this._options = Object.assign(DEFAULT_OPTIONS, options);
+		this._options = Object.assign({ ...DEFAULT_OPTIONS }, options);
 		this._event = new EventEmitter();
 	}
 
@@ -51,32 +37,25 @@ export class Retry<T extends (...args: any) => Promise<any>> {
 	 * @param tries Max number of tries before rejecting.
 	 * @returns 
 	*/
-	public async try(tries: number): Promise<T>{
-		const retry = async(i: number, target: T, ...parameters: Parameters<T>): Promise<T> => {
+	public async try(tries: number): Promise<Awaited<ReturnType<T>>>{
+		const retry = async(i: number, target: T, ...parameters: Parameters<T>): Promise<Awaited<ReturnType<T>>> => {
 			try {
-				const data = await target(...[...parameters]);
+				const data = await target(...parameters);
 				return data;
 			}
 			catch (e) {
 				this._event.emit("failure", e, i, this._options.interval);
-				if (i <= 1) return Promise.reject(e);
+				if (i <= 1) throw e;
 				this._event.emit("retry", i, this._options.interval);
 				if(this._options.interval > 0){
 					await new Promise(resolve => setTimeout(resolve, this._options.interval));
 					this._updateInterval();
-					return retry(i - 1, target, ...parameters);
 				}
-				else return retry(i - 1, target, ...parameters);
+				return retry(i - 1, target, ...parameters);
 			}
 		};
-
-		try{
-			const result = await retry(tries, this._target, ...this._parameters);
-			return result;
-		}
-		catch(e){
-			return Promise.reject(e);
-		}
+		
+		return retry(tries, this._target, ...this._parameters);
 	}
 
 	private _updateInterval(): number{
@@ -84,12 +63,30 @@ export class Retry<T extends (...args: any) => Promise<any>> {
 		return this._options.interval;
 	}
 	
+	/**
+	 * Runs the given callback function `cb` each time the specified event is triggered
+	 * @param event Event to monitor
+	 * @param cb Callback function to run
+	 * @returns 
+	 */
+	on(event: "failure", cb: (error: unknown, countdown: number, currentInterval: number) => void): this;
+	on(event: "retry", cb: (countdown: number, currentInterval: number) => void): this;
+	//eslint-disable-next-line
 	public on(event: string, cb: (...args: any) => void): this {
 		this._event.on(event, cb);
 		return this;
 	}
 
-	public once(event: string, cb: (...args: any) => void): this {
+	/**
+	 * Runs the given callback function `cb` on the first time the specified event is triggered
+	 * @param event Event to monitor
+	 * @param cb Callback function to run
+	 * @returns 
+	 */
+	once(event: "failure", cb: (error: unknown, countdown: number, currentInterval: number) => void): this
+	once(event: "retry", cb: (countdown: number, currentInterval: number) => void): this
+	//eslint-disable-next-line
+	public once(event: string, cb: (...args: any[]) => void): this {
 		this._event.once(event, cb);
 		return this;
 	}
@@ -100,16 +97,11 @@ export class Retry<T extends (...args: any) => Promise<any>> {
  * @param tries Max number of tries before rejecting.
  * @param target An async function to execute.
  * @param parameters Parameters of the async function.
+ * @param options Options for subsequent tries behavior
  * @returns A Promise resolved if the function could be executed successfully.
 */
-export default async function retry<T extends (...args: any) => Promise<any>>
-(tries: number, target: T, parameters: Parameters<T>, options: Partial<RetryOptions>){
-	try{
-		const r = new Retry(target, parameters, options);
-		const result = await r.try(tries);
-		return Promise.resolve(result);
-	}
-	catch(e: any){
-		return Promise.reject(e);
-	}
+export default function retry< T extends (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> >
+(tries: number, target: T, parameters: Parameters<T>, options?: Partial<RetryOptions>){
+	const r = new Retry(target, parameters, options);
+	return r.try(tries);
 }
